ca08abf61f9d1ae94d21eb7fb2a87834e69d3217
michaelCaleyWhaley <kneedeepwater@hotmail.com>
2021-01-31 22:21:13 +0000

    test playwright

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,18 +1,10 @@
-import chrome from "chrome-aws-lambda";
-import puppeteer from "puppeteer-core";
+import playwright from "playwright";
 
 export default async (req, res) => {
-  const browser = await puppeteer.launch({
-    args: chrome.args,
-    executablePath: await chrome.executablePath,
-    headless: chrome.headless,
-  });
-
-  // const browser = await puppeteer.launch({ headless: true });
-  const page = await browser.newPage();
-  await page.goto("https://example.com");
-  const screen = await page.screenshot({ path: "example.png" });
-
+  const browser = await playwright["chromium"].launch();
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto("http://example.com");
   await browser.close();
-  res.send(screen);
+  res.send('working');
 };
