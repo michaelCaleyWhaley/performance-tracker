d54e9acd808b48bb7a62a2dd68af4f20a398b94a
michaelCaleyWhaley <kneedeepwater@hotmail.com>
2021-01-31 21:51:00 +0000

    test puppeteer

---
+++ pages/api/test.js
@@ -0,0 +1,10 @@
+import puppeteer from "puppeteer";
+
+export default async (req, res) => {
+  const browser = await puppeteer.launch({headless: true});
+  const page = await browser.newPage();
+  await page.goto("https://example.com");
+  const screenshot = await page.screenshot({ path: "example.png" });
+  await browser.close();
+  res.send(screenshot);
+};
