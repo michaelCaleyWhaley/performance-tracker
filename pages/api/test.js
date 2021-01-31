43a0460d942e12f25e2a35fd2e310627ba1378fc
michaelCaleyWhaley <kneedeepwater@hotmail.com>
2021-01-31 21:55:15 +0000

    test puppeteer

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,10 +1,35 @@
-import puppeteer from "puppeteer";
+import puppeteer from 'puppeteer-core';
+import lighthouse from 'lighthouse';
+import { URL } from 'url';
 
-export default async (req, res) => {
-  const browser = await puppeteer.launch({headless: true});
-  const page = await browser.newPage();
-  await page.goto("https://example.com");
-  const screenshot = await page.screenshot({ path: "example.png" });
+async function getOptions() {
+  const options = {
+    args: chrome.args,
+    executablePath: await chrome.executablePath,
+    headless: chrome.headless,
+  };
+  return options;
+}
+
+async function getResult(url) {
+  const options = await getOptions();
+  const browser = await puppeteer.launch(options);
+  const { port } = new URL(browser.wsEndpoint());
+  const result = await lighthouse(url, {
+    port,
+    output: 'html',
+    logLevel: 'error',
+  });
   await browser.close();
-  res.send(screenshot);
+  return result;
+}
+
+module.exports = async (req, res) => {
+  const urlToBeAudited = 'https://example.com'
+  const result = await getResult(urlToBeAudited);
+  if (req && result && result.lhr && result.lhr.categories) {
+    res.end('Audit done.');
+  } else {
+    res.end('result is empty');
+  }
 };
