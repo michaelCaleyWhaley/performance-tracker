ff0b92d75a125c776b25e9de877f78fc98b1c82e
michaelCaleyWhaley <kneedeepwater@hotmail.com>
2021-01-31 22:14:26 +0000

    test lambda chrome

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,7 +1,14 @@
-import puppeteer from "puppeteer";
+import chrome from "chrome-aws-lambda";
+import puppeteer from "puppeteer-core";
 
 export default async (req, res) => {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({
+    args: chrome.args,
+    executablePath: await chrome.executablePath,
+    headless: chrome.headless,
+  });
+
+  // const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
   await page.goto("https://example.com");
   const screen = await page.screenshot({ path: "example.png" });
