99debe491e9cf04ba611e82bf3a1a71d064ef58f
michaelCaleyWhaley <kneedeepwater@hotmail.com>
2021-02-01 19:46:52 +0000

    remove post requirement

--- controllers/audit.js
+++ controllers/audit.js
@@ -2,10 +2,10 @@ import reportGenerator from "./helpers/reportGenerator";
 import saveReport from "./helpers/saveReport";
 
 export default async (req, res) => {
-  if (req.method !== "POST" || req.body.audit !== true) {
-    res.send();
-    return;
-  }
+  // if (req.method !== "POST" || req.body.audit !== true) {
+  //   res.send();
+  //   return;
+  // }
 
   const report = await reportGenerator();
   await saveReport(report);
